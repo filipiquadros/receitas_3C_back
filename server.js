ec1bb5621a9f5dbe7792ec8c7efd1e10fb2b2735
filipiquadros <filipi_quadros@estudante.sesisenai.org.br>
2026-03-02 11:53:37 -0300

    Adicionamos o Sql e cadastramos um usuario

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 import Fastify  from "fastify";
+import { Pool } from "pg";
+
+const sql = new Pool({
+    user: "postgres",
+    password: "1234",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
+})
 
 const servidor = Fastify()
 
-servidor.get("/usuarios", () => {
-    return 'Esta Funcionando'
+servidor.get("/usuarios", async () => {
+    const result = await sql.query('SELECT * FROM usuario')
+    return result.rows
+})
+
+servidor.post('/usuarios', async (request, reply) => {
+    const nome = request.body.nome;
+    const senha = request.body.senha;
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)',  [nome, senha]);
+    return 'Usuario Cadastrado'
 })
 servidor.listen({port: 3000})
