5b408557f7c3023a4e2ed5fcb05e5d76899aaa0f
filipiquadros <filipi_quadros@estudante.sesisenai.org.br>
2026-03-04 08:52:34 -0300

    Adicionamos o Sql e cadastramos um usuario

--- server.js
+++ server.js
@@ -22,4 +22,12 @@ servidor.post('/usuarios', async (request, reply) => {
     const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)',  [nome, senha]);
     return 'Usuario Cadastrado'
 })
+
+servidor.put('/usuarios/:id', async (request, reply) => {
+    const body = request.body;
+    const id = request.params.id;
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [body.nome, body.senha, id])
+    return 'Usuario Alterado!';
+})
+
 servidor.listen({port: 3000})
