3bac82458116026cbbcf3c91004c88b02598b236
filipiquadros <filipi_quadros@estudante.sesisenai.org.br>
2026-03-16 11:57:13 -0300

    Adicionou o email

--- server.js
+++ server.js
@@ -19,11 +19,12 @@ servidor.get("/usuarios", async () => {
 servidor.post('/usuarios', async (request, reply) => {
     const nome = request.body.nome;
     const senha = request.body.senha;
+    const email = request.body.email;
     
-    if (!nome || !senha) {
-    reply.status(400).send({ error : 'Nome ou senha Invalidos!'})
+    if (!nome || !senha  || !email) {
+    reply.status(400).send({ error : 'Nome, senha, email são obrigatórios!'})
     }
-    const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)',  [nome, senha]);
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha, email) VALUES ($1, $2, $3)',  [nome, senha, email]);
     reply.status(201).send({message: 'Usuario Cadastrado!'})
 })
 
@@ -31,8 +32,8 @@ servidor.put('/usuarios/:id', async (request, reply) => {
     const body = request.body;
     const id = request.params.id;
 
-    if (!body.nome || !body.senha) {
-        reply.status(400).send({ error : 'Nome ou senha Invalidos!'})
+    if (!body.nome || !body.senha || !body.email) {
+        reply.status(400).send({ error : 'Nome, senha, email são obrigatórios!'})
     } else if (!id) {
         reply.status(400).send({ error : 'Faltou o ID!'})
     }
@@ -41,7 +42,7 @@ servidor.put('/usuarios/:id', async (request, reply) => {
     if (existe.rows.length === 0) {
         reply.status(400).send({ error : 'Usuario Não Existe no Banco!'})
     }
-    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [body.nome, body.senha, id])
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2, email = $3 WHERE id = $4', [body.nome, body.senha, body.email, id])
     return 'Usuario Alterado!';
 })
 
@@ -57,5 +58,22 @@ servidor.delete('/usuarios/:id', async (request, reply) => {
     reply.status(204)
 
 })
+
+
+
+servidor.post('/login', async (request, reply) => {
+    const email = request.body.email;
+    const senha = request.body.senha;
+    if (!email || !senha) {
+        reply.status(400).send({ error : 'Email ou senha Invalidos!'})
+    }
+    const resultado = await sql.query('SELECT * FROM usuario WHERE email = $1 AND senha = $2', [email, senha])
+    if (resultado.rows.length === 0) {
+        reply.status(400).send({ error : 'Email ou senha Invalidos!'})
+    } else {
+        reply.send({ message : 'Login realizado com sucesso!' })
+    }
+})
+
 
 servidor.listen({port: 3000})
